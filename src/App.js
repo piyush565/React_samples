49401e6a4ac2f2c932141625fd00b4701a24ef8e
Piyush Soni <piyushsoni.one@gmail.com>
2022-06-29 19:37:38 +0530

    Added filter search feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const list = [
+    "Banana",
+    "Apple",
+    "Orange",
+    "Mango",
+    "Pineapple",
+    "Watermelon"
+  ];
+  const [filterList, setFilterList] = useState(list);
+
+  const handleSearch = (event) => {
+    if (event.target.value === "") {
+      setFilterList(list);
+      return;
+    }
+
+    const filteredValues = list.filter(
+      (item) =>
+        item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+    );
+    setFilterList(filteredValues);
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div>
+        {" "}
+        Search :{" "}
+        <input text="name" name="query" onChange={handleSearch}></input>
+      </div>
+      {filterList &&
+        filterList.map((item, index) => <div key={index}>{item}</div>)}
+      <h2> Search filter in React </h2>
     </div>
   );
 }
